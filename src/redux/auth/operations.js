64e48f1f85151e29517b99dfc01205aeeefbe2d4
Veronika Kravchenko <aniregma@gmail.com>
2022-12-04 18:18:32 +0200

    Update operations.js

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -3,12 +3,10 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
-// Utility to add JWT
 const setAuthHeader = token => {
     axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
 
-// Utility to remove JWT
 const clearAuthHeader = () => {
     axios.defaults.headers.common.Authorization = '';
 };
